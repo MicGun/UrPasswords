801dbf2dafd937a3587b3e44b6fd37d50b98d415
Hugh Wang <hugh@easychat.co>
2022-06-03 00:00:04 +0800

    add dependencies

--- app/src/main/java/com/example/urpasswords/MainActivity.kt
+++ app/src/main/java/com/example/urpasswords/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.urpasswords
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.urpasswords.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
